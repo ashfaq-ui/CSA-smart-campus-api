bd93f740dc183302734d487942e5993e6d1c7f89
ashfaq-ui <rizvimahamed456@gmail.com>
2026-04-13 20:05:10 +0530

    update GenericExceptionMapper to return clean 500 JSON response

--- src/main/java/com/smartcampus/exception/GenericExceptionMapper.java
+++ src/main/java/com/smartcampus/exception/GenericExceptionMapper.java
@@ -1,17 +1,30 @@
 package com.smartcampus.exception;
 
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @Provider
 public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 
+    private static final Logger LOGGER =
+            Logger.getLogger(GenericExceptionMapper.class.getName());
+
     @Override
     public Response toResponse(Throwable ex) {
+        LOGGER.severe("Unexpected error: " + ex.getMessage());
         ex.printStackTrace();
+
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "500 Internal Server Error");
+        error.put("message", "An unexpected error occurred");
+        error.put("hint", "Please contact the API administrator");
+
         return Response.status(500)
-                .entity("{\"error\":\"" + ex.getMessage() + "\"}")
+                .entity(error)
                 .type("application/json")
                 .build();
     }
