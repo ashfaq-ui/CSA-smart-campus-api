81d97066f26fac091a1b838638c32e1d94afec14
ashfaq-ui <rizvimahamed456@gmail.com>
2026-04-13 15:33:36 +0530

    add DiscoveryResource GET /api/v1 with HATEOAS links and API metadata

--- src/main/java/com/smartcampus/Main.java
+++ src/main/java/com/smartcampus/Main.java
@@ -1,28 +1,25 @@
-package com.smartcampus;
-
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
-
-import java.io.IOException;
-import java.net.URI;
-
-public class Main {
-
-    public static final String BASE_URI = "http://localhost:8080/";
-
-    public static HttpServer startServer() {
-        ResourceConfig config = new ResourceConfig()
-                .packages("com.smartcampus");
-        return GrizzlyHttpServerFactory.createHttpServer(
-                URI.create(BASE_URI), config);
-    }
-
-    public static void main(String[] args) throws IOException {
-        HttpServer server = startServer();
-        System.out.println("Smart Campus API running at " + BASE_URI);
-        System.out.println("Press ENTER to stop...");
-        System.in.read();
-        server.stop();
-    }
-}
+//package com.smartcampus;
+//
+//import org.glassfish.grizzly.http.server.HttpServer;
+//import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+//import org.glassfish.jersey.jackson.JacksonFeature;
+//import org.glassfish.jersey.server.ResourceConfig;
+//
+//import java.io.IOException;
+//import java.net.URI;
+//
+//public class Main {
+//
+//    public static void main(String[] args) throws IOException {
+//        ResourceConfig config = new ResourceConfig()
+//                .packages("com.smartcampus.resource")
+//                .register(JacksonFeature.class);
+//
+//        HttpServer server = GrizzlyHttpServerFactory
+//                .createHttpServer(URI.create("http://0.0.0.0:8080/"), config);
+//
+//        System.out.println("Server started at http://localhost:8080/");
+//        System.in.read();
+//        server.shutdownNow();
+//    }
+//}
--- src/main/java/com/smartcampus/resource/DiscoveryResource.java
+++ src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -8,7 +8,7 @@ import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.Map;
 
-@Path("/")
+@Path("/api/v1")
 public class DiscoveryResource {
 
     @GET
@@ -16,20 +16,14 @@ public class DiscoveryResource {
     public Response discover() {
 
         Map<String, Object> response = new HashMap<>();
-
-        // API versioning info
         response.put("api", "Smart Campus API");
         response.put("version", "v1");
         response.put("status", "running");
-
-        // Admin contact
         response.put("contact", "admin@smartcampus.ac.uk");
 
-        // HATEOAS - links to primary resource collections
         Map<String, String> links = new HashMap<>();
-        links.put("rooms",    "/api/v1/rooms");
-        links.put("sensors",  "/api/v1/sensors");
-
+        links.put("rooms", "/api/v1/rooms");
+        links.put("sensors", "/api/v1/sensors");
         response.put("resources", links);
 
         return Response.ok(response).build();
