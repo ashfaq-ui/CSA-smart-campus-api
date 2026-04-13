5deb4aafe9a277dd5e7efc64c453546bb6977098
ashfaq-ui <rizvimahamed456@gmail.com>
2026-04-13 19:33:41 +0530

    fix: add @JsonIgnoreProperties to Sensor for Jackson deserialization

--- src/main/java/com/smartcampus/model/Sensor.java
+++ src/main/java/com/smartcampus/model/Sensor.java
@@ -1,5 +1,8 @@
 package com.smartcampus.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Sensor {
 
     private String id;
