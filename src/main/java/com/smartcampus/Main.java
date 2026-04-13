1c731520cf6a73c60b39cfee85ffcdb1adf6d126
ashfaq-ui <rizvimahamed456@gmail.com>
2026-04-13 15:34:11 +0530

    fix: register JacksonFeature and use packages scan for resource discovery

--- src/main/java/com/smartcampus/Main.java
+++ src/main/java/com/smartcampus/Main.java
@@ -1,25 +1,25 @@
-//package com.smartcampus;
-//
-//import org.glassfish.grizzly.http.server.HttpServer;
-//import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-//import org.glassfish.jersey.jackson.JacksonFeature;
-//import org.glassfish.jersey.server.ResourceConfig;
-//
-//import java.io.IOException;
-//import java.net.URI;
-//
-//public class Main {
-//
-//    public static void main(String[] args) throws IOException {
-//        ResourceConfig config = new ResourceConfig()
-//                .packages("com.smartcampus.resource")
-//                .register(JacksonFeature.class);
-//
-//        HttpServer server = GrizzlyHttpServerFactory
-//                .createHttpServer(URI.create("http://0.0.0.0:8080/"), config);
-//
-//        System.out.println("Server started at http://localhost:8080/");
-//        System.in.read();
-//        server.shutdownNow();
-//    }
-//}
+package com.smartcampus;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        ResourceConfig config = new ResourceConfig()
+                .packages("com.smartcampus.resource")
+                .register(JacksonFeature.class);
+
+        HttpServer server = GrizzlyHttpServerFactory
+                .createHttpServer(URI.create("http://0.0.0.0:8080/"), config);
+
+        System.out.println("Server started at http://localhost:8080/");
+        System.in.read();
+        server.shutdownNow();
+    }
+}
