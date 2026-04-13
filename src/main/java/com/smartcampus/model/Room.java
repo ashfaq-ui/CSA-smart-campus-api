09a75a2831cdee57008005ec338ce09a08b80547
ashfaq-ui <rizvimahamed456@gmail.com>
2026-04-13 16:06:26 +0530

    add @JsonIgnoreProperties annotation to Room for Jackson deserialization

--- src/main/java/com/smartcampus/model/Room.java
+++ src/main/java/com/smartcampus/model/Room.java
@@ -1,8 +1,10 @@
 package com.smartcampus.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Room {
 
     private String id;
